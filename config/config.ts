34bfea03e1643ef286ee2f2796628243ec34bc74
Douglas Rafael <douglas.rafael@nutes.uepb.edu.br>
2018-08-24 17:36:16 -0300

    Alterado as configurações padrões no config.ts Mudado título do microservice para Account Service. Mudado descrição para Microservice for user management. Mudado nome do banco para accout.
    OBS.: Estavam as configurações defaults oriundas do template

--- config/config.ts
+++ config/config.ts
@@ -8,13 +8,12 @@ class Configs {
      * Provide configuration variables here.
      * Example: key: string = 'kEY_VALUE'
      */
-    APP_TITLE = 'Base Project Typescript'
-    APP_DESCRIPTION = 'Microservice for data collection'
-    DB_URI: string = 'mongodb://127.0.0.1:27017/base-project-ts'
-    DB_URI_TEST: string = 'mongodb://127.0.0.1:27017/base-project-ts-test'
+    APP_TITLE = 'Account Service'
+    APP_DESCRIPTION = 'Microservice for user management.'
+    DB_URI: string = 'mongodb://127.0.0.1:27017/account'
+    DB_URI_TEST: string = 'mongodb://127.0.0.1:27017/account-test'
     README_DEFAULT: string = `<h2>${this.APP_TITLE} - <small>${this.APP_DESCRIPTION}.</small></h2>
     <p>Access the API documentation <a href="/api/v1/doc" >v.1.0 </a></p>`
-
 
     jwtSecret =  "MyS3cr3tK3Y"
     jwtSession = {session: false}
